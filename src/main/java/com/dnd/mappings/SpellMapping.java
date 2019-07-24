1b3e2e6ba493c4f2c69bef7a9d916f89f49a8978
unknown <willie.c.jensen@gmail.com>
2019-07-23 17:26:27 -0700

    added unit tests + updated git ignore

--- src/main/java/com/dnd/mappings/SpellMapping.java
+++ src/main/java/com/dnd/mappings/SpellMapping.java
@@ -2,19 +2,12 @@ package com.dnd.mappings;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.dnd.beans.Spell;
-import com.dnd.services.SpellService;
 
 @Component
 public class SpellMapping {
-	
-	@Autowired
-	private SpellService spellService;
 	
 	public Spell SpellFromMap(Map<String,Object> map) {
 		Spell s = new Spell();
@@ -22,6 +15,6 @@ public class SpellMapping {
 		if(map.containsKey("spellId")) {
 			s.setSpellId((int)map.get("spellId"));
 		}
-		return spellService.addSpell(s);
+		return s;
 	}
 }
